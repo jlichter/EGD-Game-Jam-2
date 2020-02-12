dbd90318ae10ac22d49682f12e58054338b4f598
Jessie <jlichter123@gmail.com>
2020-02-12 12:02:21 -0500

    adding to level

---
+++ Assets/Scripts/PlatformAttach.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformAttach : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+
+    // when player lands on platform, attach so player wont slide off
+    private void OnTriggerStay(Collider other) {
+
+        if (other.gameObject.tag == "Player1") {
+            player1.transform.parent = transform;
+        }
+        if (other.gameObject.tag == "Player2") {
+            Debug.Log("here");
+            player2.transform.parent = transform;
+        }
+    }
+    // when either player leaves platform, detach
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player1") {
+            player1.transform.parent = null;
+        }
+        if (other.gameObject.tag == "Player2") {
+            player2.transform.parent = null;
+        }
+    }
+}
---
+++ Assets/Scripts/RotatingControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingControl : MonoBehaviour
+{
+
+    // target angle and original for rotationg platform 1 
+    Quaternion targetAngle1_90 = Quaternion.Euler(90, 0, 0);
+    Quaternion targetAngle1_0 = Quaternion.Euler(0, 0, 0);
+    public Quaternion currentAngle1;
+    // to grab the coroutine for rotation
+   // IEnumerator coroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
